91ed47025fee61179f2efe9444b0ad0f9d517106
bodia-pavuk-0xit <b.yakobchuk.workmail@gmail.com>
2023-04-13 17:38:16 +0300

    Made and adapted the "What students of our courses say" and "Video" blocks. Partly made by the block "Our Mentors"

---
+++ js/slider-testimonials.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  var $slider = $(".testimonials-block__slider");
+  $slider.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    cssEase: "linear",
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 10000,
+  });
+});
